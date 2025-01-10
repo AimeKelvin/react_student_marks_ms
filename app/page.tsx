b680c21c3f173386bc3e81e6bfdea38c3a40f73f
Shimwa Aime Kelvin <158560776+AimeKelvin@users.noreply.github.com>
2025-01-10 09:54:42 +0200

    finished initialization

--- app/page.tsx
+++ app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
             </code>
             .
           </li>
-          <li>Save and see your changes instantly.</li>
+          <li>FUCK YOUR WIFI ITS LIKE I CAME BY BUS 🥷🏾.</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
