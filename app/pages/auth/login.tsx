2ae4f804847c93dd082f41e1d0763a2e3ee4f491
Shimwa Aime Kelvin <158560776+AimeKelvin@users.noreply.github.com>
2025-01-10 11:07:46 +0200

    shadcn form auth

--- app/pages/auth/login.tsx
+++ app/pages/auth/login.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import { LoginForm } from "@/components/login-form"
 
-function login() {
+export default function Login() {
   return (
-    <div>login</div>
+    <div className="flex min-h-svh flex-col items-center justify-center bg-muted p-6 md:p-10">
+      <div className="w-full max-w-sm md:max-w-3xl">
+        <LoginForm />
+      </div>
+    </div>
   )
 }
-
-export default login
